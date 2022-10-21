a68ab6ade13be4643181daf50a881652bf27b317
Sami Alabdalah <samialabdalah@yahoo.com>
2022-10-21 22:05:11 +0200

    before responsive design for mainmenusection

---
+++ src/views/ContactsView.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import FooterSection from '../sections/FooterSection'
+import MainMenuSection from '../sections/MainMenuSection'
+
+const ContactsView = () => {
+  return (
+    <>
+      <MainMenuSection />
+      <FooterSection />
+
+    </>
+  )
+}
+
+export default ContactsView
