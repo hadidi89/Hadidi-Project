baf7165ea748edf5e31f996a50400fe225083fba
Sami Alabdalah <samialabdalah@yahoo.com>
2022-10-25 11:21:16 +0200

    Start with lecture 7

--- src/components/MenuIcon.js
+++ src/components/MenuIcon.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const MenuIcon = ({link, icon, quantity, hideOnMobile, hideOnTablet}) => {
+const MenuIcon = ({link, icon, quantity, hideOnMobile}) => {
 
   return (
-    <NavLink className={`menu-icon ${hideOnMobile ? "hideOnMobile" : ""} ${hideOnTablet ? "hideOnTablet" : ""}`} to={link} end>
+    <NavLink className={`menu-icon ${hideOnMobile ? "d-none d-md-flex" : ""}`} to={link} end>
         <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{quantity}</span>
         <i className={icon}></i>
     </NavLink>
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FooterSection from '../sections/FooterSection'
 import MainMenuSection from '../sections/MainMenuSection'
+import ProductGridSection from '../sections/ProductGridSection'
 
 const HomeView = () => {
+
+  const [featuredProducts, setFeaturedProducts] = useState([
+    { id: 1, name: "Modern Black Blouse", category: "Fashion", price: "$35.00",rating: 5, img: "https://images.pexels.com/photos/13991658/pexels-photo-13991658.jpeg?auto=compress&cs=tinysrgb&w=255&h=750&dpr=2" },
+    { id: 2, name: "Modern Black Blouse", category: "Fashion", price: "$35.00",rating: 5, img: "https://images.pexels.com/photos/11928245/pexels-photo-11928245.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" },
+    { id: 3, name: "Modern Black Blouse", category: "Fashion", price: "$35.00",rating: 5, img: "https://images.pexels.com/photos/13834131/pexels-photo-13834131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" },
+    { id: 4, name: "Modern Black Blouse", category: "Fashion", price: "$35.00",rating: 5, img: "https://images.pexels.com/photos/9969158/pexels-photo-9969158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" }
+  ])
+  const [topProducts, setTopProducts] = useState([
+    { id: 1, name: "Modern Black Blouse", category: "Fashion", price: "$35.00",rating: 5, img: "https://images.pexels.com/photos/13991658/pexels-photo-13991658.jpeg?auto=compress&cs=tinysrgb&w=255&h=750&dpr=2" },
+    { id: 2, name: "Modern Black Blouse", category: "Fashion", price: "$35.00",rating: 5, img: "https://images.pexels.com/photos/11928245/pexels-photo-11928245.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" },
+    { id: 3, name: "Modern Black Blouse", category: "Fashion", price: "$35.00",rating: 5, img: "https://images.pexels.com/photos/13834131/pexels-photo-13834131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" },
+  ])
+
   return (
     <>
       <MainMenuSection />
+      <ProductGridSection  title="Featured Products" products={featuredProducts} />
+      <ProductGridSection  title="Top Products" products={topProducts} />
       <FooterSection />
     </>
   )
